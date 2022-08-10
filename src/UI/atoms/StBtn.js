8e3ddeb0c77bae7009c75f829d706ad1fd359cf1
jhhyung <jinhahyung@gmail.com>
2022-08-10 13:29:12 +0900

    이미지박스라는 atom component를 생성하고 editform에 대한 css를 끝마쳤습니다.

--- src/UI/atoms/StBtn.js
+++ src/UI/atoms/StBtn.js
@@ -7,6 +7,7 @@ export const StBtn = styled.button`
   cursor: pointer;
 
   border-radius: 20px;
-  background-color: transparent;
+  background-color: ${(props) => props.backgroundColor || "transparent"};
+  color: ${(props) => props.color || "black"};
   border: none;
 `;
--- src/components/pages/DetailPage.js
+++ src/components/pages/DetailPage.js
@@ -3,7 +3,6 @@ import React from "react";
 import Layout from "../templates/Layout";
 import Header from "../templates/Header";
 import SingleLog from "../../UI/organisms/SingleLog";
-import Comment from "../../UI/organisms/Comment";
 
 const DetailPage = () => {
   return (
